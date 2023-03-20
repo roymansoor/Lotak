7908c1179ca7a12cf7d571e13267da62d9a4414a
roymansoor <roy.mansoor@gmail.com>
2023-03-20 16:56:03 -0500

    landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import videoBg from "/public/assets/landingBackground.mp4"
+import "../styls/LandingPage.css"
 
 function LandingPage (){
 
@@ -8,8 +9,8 @@ function LandingPage (){
             <div className='overlay'></div>
             <video src={videoBg} autoPlay loop muted/>
             <div className='Title'>
-            <h1> welcome to lotak</h1>
-            <p>hello hi</p>
+            <h1 className='LandingPage-Title-Welcome'> LOTAK</h1>
+            <p className='LandingPage-Title-Ptag'>One Stop Solution for all your Real Estate needs...</p>
             </div>
         </div>
     )
